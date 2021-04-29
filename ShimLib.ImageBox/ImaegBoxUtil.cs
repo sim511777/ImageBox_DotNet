940b96e19f4611e6ce59d29924042a6f4623b76b
sim511777 <sim51177@gmail.com>
2021-04-29 22:58:15 +0900

    1Bit Bitmap 버퍼 로딩 기능 구현

--- ShimLib.ImageBox/ImaegBoxUtil.cs
+++ ShimLib.ImageBox/ImaegBoxUtil.cs
@@ -89,6 +89,11 @@ namespace ShimLib {
         }
 
         public unsafe static void BitmapToImageBuffer(Bitmap bmp, ref IntPtr imgBuf, ref int bw, ref int bh, ref int bytepp) {
+            if (bmp.PixelFormat == PixelFormat.Format1bppIndexed) {
+                Bitmap1BitToGrayImageBuffer(bmp, ref imgBuf, ref bw, ref bh, ref bytepp);
+                return;
+            }
+
             bw = bmp.Width;
             bh = bmp.Height;
             if (bmp.PixelFormat == PixelFormat.Format8bppIndexed)
@@ -114,6 +119,11 @@ namespace ShimLib {
         }
 
         public unsafe static void BitmapToGrayImageBuffer(Bitmap bmp, ref IntPtr imgBuf, ref int bw, ref int bh, ref int bytepp) {
+            if (bmp.PixelFormat == PixelFormat.Format1bppIndexed) {
+                Bitmap1BitToGrayImageBuffer(bmp, ref imgBuf, ref bw, ref bh, ref bytepp);
+                return;
+            }
+
             bw = bmp.Width;
             bh = bmp.Height;
             if (bmp.PixelFormat == PixelFormat.Format8bppIndexed)
@@ -144,6 +154,37 @@ namespace ShimLib {
                         gray = (byte)((3 * r + 6 * g + 1 * b) / 10);
                     }
                     *dstPtr = gray;
+                }
+            }
+
+            bmp.UnlockBits(bmpData);
+
+            bytepp = 1;
+        }
+
+        private unsafe static void Bitmap1BitToGrayImageBuffer(Bitmap bmp, ref IntPtr imgBuf, ref int bw, ref int bh, ref int bytepp) {
+            bw = bmp.Width;
+            bh = bmp.Height;
+            Int64 bufSize = (Int64)bw * bh;
+            imgBuf = AllocBuffer(bufSize);
+
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bw, bh), ImageLockMode.ReadOnly, bmp.PixelFormat);
+            for (int y = 0; y < bh; y++) {
+                byte* dstPtr = (byte*)imgBuf + bw * y;
+                byte* srcPtr = (byte*)bmpData.Scan0 + bmpData.Stride * y;
+                
+                int stepNum = bw / 8;
+                int stepRemains = bw % 8;
+                int val = 0;
+                for (int step = 0; step < stepNum; step++, dstPtr += 8, srcPtr++) {
+                    val = *srcPtr;
+                    for (int bit = 0; bit < 8; bit++) {
+                        dstPtr[bit] = (byte)((val >> (7 - bit) & 1) * 255);
+                    }
+                }
+                val = *srcPtr;
+                for (int bit = 0; bit < stepRemains; bit++) {
+                    dstPtr[bit] = (byte)((val >> (7 - bit) & 1) * 255);
                 }
             }
 
