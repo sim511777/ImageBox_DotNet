7c4e9004c9835603ad50a0ca3329a7110ed3d847
sim511777 <sim51177@gmail.com>
2021-05-02 00:57:16 +0900

    DrawString 위치 변경

--- ImageBox_Test/FormMain.cs
+++ ImageBox_Test/FormMain.cs
@@ -26,7 +26,7 @@ namespace ImageBox_Test {
             id.DrawRectangle(Color.Red, 8, 8, 4, 4);
             id.DrawRectangle(Color.Red, 16.5f, 16.5f, 4f, 4f);
 
-            id.DrawString(tbxExample.Text, (BitmapFont)cbxFont.SelectedValue, Color.Lime, 12, 12);
+            id.DrawString(tbxExample.Text, (BitmapFont)cbxFont.SelectedValue, Color.Lime, 50, 50);
 
             //for (int i = 0; i < 100; i++) {
             //    for (int j = 0; j < 100; j++) {
