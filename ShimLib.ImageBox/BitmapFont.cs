6c4486fda972cafd9b986efe3b01f4bd43941e2f
sim511777 <sim51177@gmail.com>
2021-05-02 06:38:41 +0900

    유니코드폰트 표시 구현

--- ShimLib.ImageBox/BitmapFont.cs
+++ ShimLib.ImageBox/BitmapFont.cs
@@ -9,44 +9,57 @@ using ShimLib.Properties;
 
 namespace ShimLib {
     public class BitmapFonts {
-        public static BitmapFont Ascii_04x06 = new BitmapFont(Resource.Raster_04x06);
-        public static BitmapFont Ascii_05x08 = new BitmapFont(Resource.Raster_05x08);
-        public static BitmapFont Ascii_05x12 = new BitmapFont(Resource.Raster_05x12);
-        public static BitmapFont Ascii_06x08 = new BitmapFont(Resource.Raster_06x08);
-        public static BitmapFont Ascii_06x13 = new BitmapFont(Resource.Raster_06x13);
-        public static BitmapFont Ascii_07x12 = new BitmapFont(Resource.Raster_07x12);
-        public static BitmapFont Ascii_08x08 = new BitmapFont(Resource.Raster_08x08);
-        public static BitmapFont Ascii_08x12 = new BitmapFont(Resource.Raster_08x12);
-        public static BitmapFont Ascii_08x16 = new BitmapFont(Resource.Raster_08x16);
-        public static BitmapFont Ascii_08x18 = new BitmapFont(Resource.Raster_08x18);
-        public static BitmapFont Ascii_10x18 = new BitmapFont(Resource.Raster_10x18);
-        public static BitmapFont Ascii_10x20 = new BitmapFont(Resource.Raster_10x20);
-        public static BitmapFont Ascii_10x22 = new BitmapFont(Resource.Raster_10x22);
-        public static BitmapFont Ascii_12x16 = new BitmapFont(Resource.Raster_12x16);
-        public static BitmapFont Ascii_12x27 = new BitmapFont(Resource.Raster_12x27);
-        public static BitmapFont Ascii_16x08 = new BitmapFont(Resource.Raster_16x08);
-        public static BitmapFont Ascii_16x12 = new BitmapFont(Resource.Raster_16x12);
+        public static BitmapFont Ascii_04x06 = new BitmapFont(Resource.Raster_04x06, 04, 06, 0, 0, 32);
+        public static BitmapFont Ascii_05x08 = new BitmapFont(Resource.Raster_05x08, 05, 08, 0, 0, 32);
+        public static BitmapFont Ascii_05x12 = new BitmapFont(Resource.Raster_05x12, 05, 12, 0, 0, 32);
+        public static BitmapFont Ascii_06x08 = new BitmapFont(Resource.Raster_06x08, 06, 08, 0, 0, 32);
+        public static BitmapFont Ascii_06x13 = new BitmapFont(Resource.Raster_06x13, 06, 13, 0, 0, 32);
+        public static BitmapFont Ascii_07x12 = new BitmapFont(Resource.Raster_07x12, 07, 12, 0, 0, 32);
+        public static BitmapFont Ascii_08x08 = new BitmapFont(Resource.Raster_08x08, 08, 08, 0, 0, 32);
+        public static BitmapFont Ascii_08x12 = new BitmapFont(Resource.Raster_08x12, 08, 12, 0, 0, 32);
+        public static BitmapFont Ascii_08x16 = new BitmapFont(Resource.Raster_08x16, 08, 16, 0, 0, 32);
+        public static BitmapFont Ascii_08x18 = new BitmapFont(Resource.Raster_08x18, 08, 18, 0, 0, 32);
+        public static BitmapFont Ascii_10x18 = new BitmapFont(Resource.Raster_10x18, 10, 18, 0, 0, 32);
+        public static BitmapFont Ascii_10x20 = new BitmapFont(Resource.Raster_10x20, 10, 20, 0, 0, 32);
+        public static BitmapFont Ascii_10x22 = new BitmapFont(Resource.Raster_10x22, 10, 22, 0, 0, 32);
+        public static BitmapFont Ascii_12x16 = new BitmapFont(Resource.Raster_12x16, 12, 16, 0, 0, 32);
+        public static BitmapFont Ascii_12x27 = new BitmapFont(Resource.Raster_12x27, 12, 27, 0, 0, 32);
+        public static BitmapFont Ascii_16x08 = new BitmapFont(Resource.Raster_16x08, 16, 08, 0, 0, 32);
+        public static BitmapFont Ascii_16x12 = new BitmapFont(Resource.Raster_16x12, 16, 12, 0, 0, 32);
+        public static BitmapFont Unicode_16x12 = new BitmapFont(Resource.Unifont_16x16, 16, 16, 32, 64, 0);
     }
 
     public class BitmapFont {
         private IntPtr fontBuf;
         private int fontBw;
         private int fontBh;
+
         private int fw;
         private int fh;
-        private int bytepp;
+        private int bufSx;
+        private int bufSy;
+        private int nx;
+        private int ny;
+        private ushort charSI;
+        private ushort charEI;
 
-        private int fontBufStartX;
-        private int fontBufStartY;
-        private int fontStartIndex;
-        public BitmapFont(Bitmap bmp, int fontBufStartX, int fontBufStartY, int fontBw, int fontBh, int fontStartIndex) {
 
-        }
-
-        public BitmapFont(Bitmap bmp) {
+        public BitmapFont(Bitmap bmp, int fw, int fh, int bufSx, int bufSy, ushort charSI) {
+            int bytepp = 0;
             Util.BitmapToGrayImageBuffer(bmp, ref fontBuf, ref fontBw, ref fontBh, ref bytepp);
-            fw = fontBw / 32;
-            fh = fontBh / 3;
+            this.fw = fw;
+            this.fh = fh;
+            this.bufSx = bufSx;
+            this.bufSy = bufSy;
+            this.charSI = charSI;
+
+            int useBw = fontBw - bufSx;
+            int useBh = fontBh - bufSy;
+
+            this.nx = useBw / fw;
+            this.ny = useBh / fh;
+
+            charEI = (ushort)(charSI + nx * ny - 1);
         }
         
         ~BitmapFont() {
@@ -66,10 +79,10 @@ namespace ShimLib {
                     y += fh;
                     continue;
                 }
-                if (ch >= 32 && ch <= 128) {
-                    int fontX = (ch - 32) * fw;
-                    int fontImgY = fontX / fontBw * fh;
-                    int fontImgX = fontX % fontBw;
+                if (ch >= charSI && ch <= charEI) {
+                    int fontIdx = ch - charSI;
+                    int fontImgY = fontIdx / nx * fh + bufSy;
+                    int fontImgX = fontIdx % nx * fw + bufSx;
                     DrawChar(fontImgX, fontImgY, dispBuf, dispBW, dispBH, x, y, icolor);
                 }
                 x += fw;
@@ -110,8 +123,8 @@ namespace ShimLib {
                 if (dy + y < 0 || dy + y >= dispBH)
                     continue;
                 int* dst = (int*)dispBuf + dispBW * (dy + y) + dx;
-                byte* src = (byte*)fontBuf + (fontBw * (fontImgY + y) + fontImgX) * bytepp;
-                for (int x = 0; x < fw; x++, src += bytepp, dst++) {
+                byte* src = (byte*)fontBuf + fontBw * (fontImgY + y) + fontImgX;
+                for (int x = 0; x < fw; x++, src++, dst++) {
                     if (dx + x < 0 || dx + x >= dispBW)
                         continue;
                     if (*src == 0) {
